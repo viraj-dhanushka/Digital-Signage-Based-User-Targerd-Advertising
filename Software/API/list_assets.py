25aa575efcb35de3475de7cc92d6bcfe5c14b868
viradhanus <e16083@eng.pdn.ac.lk>
2020-12-03 03:02:58 +0530

    simple programme to list assets

---
+++ Software/API/list_assets.py
@@ -0,0 +1,18 @@
+#simple python programme to list assets
+import requests
+
+ip = input("Enter your ip: ")
+# ip = "192.168.1.101"
+
+
+import requests
+headers = {
+    "Authorization": "Token {token}",
+    "Content-Type": "application/json"
+}
+response = requests.request(
+    method='GET',
+    url='http://' + ip + '/api/v1.2/assets',
+    headers=headers
+)
+print(response.json())

ebf40812aa19ea8bef766e3a05344f783aab39c7
Emyll Almonte <eg0dz@aol.com>
2020-12-04 17:06:58 -0500

    Update list_assets.py

--- Software/API/list_assets.py
+++ Software/API/list_assets.py
@@ -1,3 +1,9 @@
+"""
+Should you add any of the encoding to the top of this file?
+https://docs.python.org/3/tutorial/interpreter.html#source-code-encoding
+You can erase this after you decide this.
+"""
+
 #simple python programme to list assets
 import requests
 

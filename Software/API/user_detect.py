683d5b0b02b6b0c6a1f383983bba00b4b1528b50
viradhanus <e16083@eng.pdn.ac.lk>
2021-01-27 16:12:23 +0530

    code repaired

--- Software/API/user_detect.py
+++ Software/API/user_detect.py
@@ -24,11 +24,6 @@ male_ads_dict = {}
 female_ads_dict = {}
 generic_ads_dict = {}
 
-
-
-
-
-
 #function to get ads from firestore
 def getAds(male_ads_dict, female_ads_dict, generic_ads_dict):
     # macAddr = getmac.get_mac_address()
@@ -38,51 +33,37 @@ def getAds(male_ads_dict, female_ads_dict, generic_ads_dict):
     rpi_mac = rpi_ref.get()
 
     if rpi_mac.exists: 
-
-        ads_ref = rpi_ref.collection(u'advertisements')
-
-        # male_ads_ref = ads_ref.document(u'male')
-        # female_ads_ref = ads_ref.document(u'female')
-        # generic_ads_ref = ads_ref.document(u'generic')
-        
-        # Create an Event for notifying main thread.
-        callback_done = threading.Event()
-
-        # Create a callback on_snapshot function to capture changes
-        def on_snapshot(col_snapshot, changes, read_time):
-            print(u'Callback received query snapshot.')
-            print(u'Current cities in California:')
-            for doc in col_snapshot:
-                if doc.id == 'male':
-                    male_ads_dict = doc.to_dict()
-                    print(male_ads_dict)
-                if doc.id == 'female':
-                    female_ads_dict = doc.to_dict()
-                if doc.id == 'generic':
-                    generic_ads_dict = doc.to_dict()
-
-            callback_done.set()
-
-        col_query = ads_ref
-
-        # Watch the collection query
-        query_watch = col_query.on_snapshot(on_snapshot)
-
-        # male_ads = male_ads_ref.get()
-        # female_ads = female_ads_ref.get()
-        # generic_ads = generic_ads_ref.get()
-
-        # if male_ads.exists:
-        #     male_ads_dict = male_ads.to_dict()
-        # if female_ads.exists:
-        #     female_ads_dict = female_ads.to_dict()
-        # if generic_ads.exists:
-        #     generic_ads_dict = generic_ads.to_dict()
-
+        print("device exists")
     else:
         rpi_ref.set({
             u'manufactured date': datetime.datetime.now(),
         })
+
+    ads_ref = rpi_ref.collection(u'advertisements')
+    
+    # Create an Event for notifying main thread.
+    callback_done = threading.Event()
+
+    # Create a callback on_snapshot function to capture changes
+    def on_snapshot(col_snapshot, changes, read_time):
+        print(u'Callback received query snapshot.')
+        print(u'Current cities in California:')
+        for doc in col_snapshot:
+            if doc.id == 'male':
+                male_ads_dict = doc.to_dict()
+                print(male_ads_dict)
+            if doc.id == 'female':
+                female_ads_dict = doc.to_dict()
+            if doc.id == 'generic':
+                generic_ads_dict = doc.to_dict()
+
+        callback_done.set()
+
+    col_query = ads_ref
+
+    # Watch the collection query
+    query_watch = col_query.on_snapshot(on_snapshot)
+
   
 # users_ref = db.collection(u'users')
 # docs = users_ref.stream()
